ba7f4dfaea3b647197e96f34e8f5bc015da2eb3e
Moealsir <mohamedwdalsir@gmail.com>
2023-12-27 14:08:15 +0300

    updated backup

--- backup.py
+++ backup.py
@@ -1,10 +1,19 @@
-import shutil  # Add the import statement for shutil
+import os
+import shutil
+from datetime import datetime
 
 def create_backup(file_path):
     try:
-        # Create a backup copy of the original file
-        backup_path = file_path + '.bak'
+        # Get the current timestamp
+        timestamp = datetime.now().strftime("%H:%M:%S")
+
+        # Create a backup copy of the original file with a timestamp in the filename
+        backup_filename = f"{os.path.basename(file_path)}_{timestamp}.bak"
+        backup_path = os.path.join(os.path.dirname(file_path), backup_filename)
+
         shutil.copy2(file_path, backup_path)
+
+        print(f"Backup created: {backup_path}")
     except FileNotFoundError:
         print(f"Error creating backup for {file_path}: File not found.")
     except Exception as e:
