884e68144f0499a405f14c5985f786af59833cdc
malazmuzamil98 <malazmuzamil98@gmail.com>
2023-12-28 16:15:49 +0200

    test file

---
+++ cp_test.c
@@ -0,0 +1,17 @@
+int func0 (int var, __attribute__((unused)) int test) 
+{
+	int t ;
+
+	t = sizeof (var);
+	return(t); 
+}
+
+int func1 (int argc, __attribute__((unused)) char** argv) 
+{
+	return(0) ; 
+}
+  
+char func2 (__attribute__((unused)) int var1, char*var2)  
+{
+	return(0) ; 
+}  
