5f0cd132cfb47cdbdec01a378495ec1083feb2dc
Moealsir <mohamedwdalsir@gmail.com>
2024-01-01 21:45:04 +0300

    brace go to next line

--- test.c
+++ test.c
@@ -1,16 +1,51 @@
 
-int func1(){
-	do{
+/**
+ * func1 - a Function that ...
+ * Return: Description of the return value.
+ */
+int func1(void)
+{
+	do {
 		return (0);
-	} while (a<b);
+	} while (a < b);
 
 	return (0);
 }
 
-int func2(){
-	do{
-		return (0); } while (a<b);
+/**
+ * func2 - a Function that ...
+ * Return: Description of the return value.
+ */
+int func2(void)
+{
+	do {
+		return (0); } while (a < b);
 
 	return (0);
 }
-s
+
+/**
+ * func3 - a Function that ...
+ * Return: Description of the return value.
+ */
+int func3(void)
+{
+	do {
+		return (0);
+	} while (a < b);
+
+	return (0);
+}
+
+/**
+ * func4 - a Function that ...
+ * Return: Description of the return value.
+ */
+int func4(void)
+{
+	do {
+		return (0); } while (a < b);
+
+	return (0);
+}
+
