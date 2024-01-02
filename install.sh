2b5549deb7cdcf1189efe022e5561fc90b2db33c
Moealsir <mohamedwdalsir@gmail.com>
2024-01-02 10:03:34 +0300

    Updated install.sh

--- install.sh
+++ install.sh
@@ -15,7 +15,9 @@ cp -r fixer/ /bin/
 
 # # Add an alias to .bashrc
 echo "alias fix='python3 /bin/fixer/betty_fixer.py'" >> ~/.bashrc
-clear
+
+# Clear the terminal
+clear 
 
 # Inform the user about the changes
 echo -e "Please copy and run this command:\n"
@@ -32,7 +34,7 @@ clear
 
 # Display a warning message
 echo "#############################################################"
-echo -e "#                     \e[1;33mWARNING\e[0m                               #"
+echo -e "#                        \e[1;33mWARNING\e[0m                            #"
 echo "#   This program will overwrite your files.                 #"
 echo "#   But ...                                                 #"
 echo "#   It will create a backup file. If you run the program    #
@@ -42,11 +44,10 @@ echo "#############################################################"
 # Ensure the user has read the warning
 read -p "Press [Enter] key to continue ..."
 
-# Enhanced outro message
 clear
 echo -e "\n\e[1;35m###########################################################\e[0m"
 echo -e "\e[1;32m       Program Successfully Installed and Ready to Go!\e[0m\n"
-echo -e "                 \e[1;36mCoded by: Moealsir\e[0m"
+echo -e "            \e[1;36mCoded by: Moealsir && malazmuzamil\e[0m"
 echo -e "  Thank you for choosing this Fixer. "
 echo -e "                               \e[1;34mEnjoy your experience!!\e[0m"
 echo -e "\e[1;35m###########################################################\e[0m\n"
