8ac2f40eed48ad2a169b97e2b0306552d514f82b
be-great <begreat5333@gmail.com>
2024-01-07 17:55:45 +0300

    testing

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 from setuptools import setup, find_packages
+from setuptools.command.install import install
 
 setup(
     name='bettyfixer',
     version='0.1',
     packages=find_packages(),
+    package_data={
+        'bettyfixer': ['install_dependency/.Betty/install.sh', 'install_dependency/.betty'],
+    },
     entry_points={
         'console_scripts': [
             'bettyfixer = bettyfixer.betty_fixer:main',
