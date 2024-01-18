b4fbeeb96ad0abce48c551ba2ddefc9d206461b3
Moealsir <mohamedwdalsir@gmail.com>
2024-01-18 06:14:53 +0300

    setup.py

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ class CustomInstallCommand(install):
 
 setup(
     name='bettyfixer',
-    version='1.3.1',
+    version='1.3.2',
     packages=find_packages(),
     cmdclass={'install': CustomInstallCommand},  # Use the custom install command
     entry_points={
