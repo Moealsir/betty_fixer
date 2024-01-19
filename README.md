d8b52c23cb7f7e8a82b154f90783b45b2a78ec91
Moealsir <mohamedwdalsir@gmail.com>
2024-01-20 01:54:19 +0300

    update setup to install black

--- README.md
+++ README.md
@@ -14,7 +14,9 @@ Before using Betty Fixer, ensure you have the following installed:
 
 - [Betty](https://github.com/holbertonschool/Betty) - The Betty linter for C code.
 - [Vi Editor](https://www.vim.org/) - The Vi editor for script execution.
+- <span style="color:#a93226;">Ctags</span> :- 
 
+        sudo apt-get install exuberant-ctags
 ## Getting Started
 
 1. Clone the repository:
@@ -34,13 +36,16 @@ Before using Betty Fixer, ensure you have the following installed:
     ```bash
     bettyfixer -H <header_name>.h ...
     ```
+## Compatibility:
+
+The current release of `bettyfixer` is optimized for Ubuntu 20.04 LTS (Focal Fossa). We are actively working to expand compatibility to include other Ubuntu releases in future updates. Stay tuned for upcoming releases that will offer support for a broader range of Ubuntu versions.
 
 
 ## Contributing
 
 If you'd like to contribute to Betty Fixer, please follow these steps:
 
-1. Go to the github repository.
+1. Go to the [Github repository](https://github.com/Moealsir/betty_fixer)
 2. Fork the repository.
 3. Create a new branch for your feature or bug fix.
 4. Make your changes and ensure the code style follows the project conventions.
@@ -48,10 +53,10 @@ If you'd like to contribute to Betty Fixer, please follow these steps:
 6. Create a pull request with a clear description of your changes.
 
 
-Happy coding!
 
 
 
 ### Creaters: - 
 [@Moealsir](https://github.com/Moealsir) <br>
-[@malazmuzamil98](https://github.com/malazmuzamil98)
+[@Malazmuzamil98](https://github.com/malazmuzamil98)<br>
+[@AhedEisa](https://github.com/be-great)
--- setup.py
+++ setup.py
@@ -4,58 +4,12 @@ from pathlib import Path
 import subprocess
 import time
 import shutil
-
-class CustomInstallCommand(install):
-    def run(self):
-        install.run(self)
-
-        # Check if Betty is installed
-        betty_executable = shutil.which('betty')
-        if betty_executable is None:
-            print("Betty not found. Installing...")
-            self.install_betty()
-
-        # Check if exuberant-ctags is installed, if not install it
-        ctags_executable = shutil.which('ctags')
-        if ctags_executable is None:
-            print("Exuberant-ctags not found. Installing...")
-            self.install_ctags()
-
-    def install_betty(self):
-        try:
-            # Clone Betty repository
-            subprocess.run(["git", "clone", "https://github.com/alx-tools/Betty.git"])
-
-            # Wait until cloning finishes
-            cloned_directory = Path("Betty")
-            while not cloned_directory.exists():
-                time.sleep(1)  # Adjust the sleep interval as needed
-            
-            # Run Betty install script
-            subprocess.run(["sudo", "./Betty/install.sh"])
-
-            # Download and copy .betty file
-            subprocess.run(["wget", "https://github.com/Moealsir/betty_fixer/raw/main/bettyfixer/install_dependency/.betty"])
-            subprocess.run(["sudo", "cp", ".betty", "/bin/betty"])
-
-            # Clean up
-            subprocess.run(["rm", "-rf", "Betty", ".betty"])
-        except Exception as e:
-            print(f"Error installing Betty: {e}")
-
-    def install_ctags(self):
-        try:
-            # Install exuberant-ctags
-            subprocess.run(["sudo", "apt-get", "install", "exuberant-ctags"])
-        except Exception as e:
-            print(f"Error installing exuberant-ctags: {e}")
-
  
 setup(
     name='bettyfixer',
-    version='1.4.5.3',
+    version='1.4.6',
     packages=find_packages(),
-    cmdclass={'install': CustomInstallCommand},  # Use the custom install command
+
     entry_points={
         'console_scripts': [
             'bettyfixer = bettyfixer.betty_fixer:main',
