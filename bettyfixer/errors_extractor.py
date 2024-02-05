592b4a44e0b828d98cb70e69d6d76388cecb5912
Younis <23105954+Younis-Ahmed@users.noreply.github.com>
2024-02-05 07:31:04 +0000

    Fix error handling in errors_extractor.py

--- bettyfixer/errors_extractor.py
+++ bettyfixer/errors_extractor.py
@@ -25,7 +25,7 @@ def exctract_errors(file_path, output_file):
             errors_file_path.write(output)
     except subprocess.CalledProcessError as e:
         # Handle the case when Betty returns a non-zero exit code
-        pass
+        pass # ❗
         # Append the error output to the common errors.txt file
         with open(output_file, 'a', encoding='utf-8') as errors_file_path:
             errors_file_path.write(e.stdout)
@@ -39,11 +39,11 @@ if __name__ == "__main__":
         sys.exit(1)
 
     # Specify the common errors.txt file in the current directory
-    errors_file_path = 'errors.txt'
+    ERROR_FILE_PATH = 'errors.txt'
 
     # Clear the content of the errors.txt file before appending new errors
-    open(errors_file_path, 'w', encoding='utf-8').close()
+    open(ERROR_FILE_PATH, 'w', encoding='utf-8').close()
 
     # Iterate over each file provided as a command-line argument
-    for file_path in sys.argv[1:]:
-        exctract_errors(file_path, errors_file_path)
+    for FILE_PATH in sys.argv[1:]:
+        exctract_errors(FILE_PATH, ERROR_FILE_PATH)
