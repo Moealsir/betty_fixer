717cb0de37c3fb2efe996f0f2e4bcc2d77cb9258
jonaahmed <23105954+jonaahmed@users.noreply.github.com>
2024-02-07 10:39:04 +0300

    Refactor error extraction function

--- bettyfixer/errors_extractor.py
+++ bettyfixer/errors_extractor.py
@@ -7,10 +7,12 @@ import sys
 
 def exctract_errors(file_path, output_file):
     """
-    Extract errors from a file using Betty style checker and append them to common errors.txt file.
+    Extract errors from a file using Betty style checker and append them
+        to common errors.txt file.
     Args:
         file_path (str): The path of the file to extract errors from.
-        output_file (str): The path of the common errors.txt file to append the errors to.
+        output_file (str): The path of the common errors.txt file to append the
+            errors to.
     """
     try:
         # Run Betty on the specified file
@@ -25,7 +27,7 @@ def exctract_errors(file_path, output_file):
             errors_file_path.write(output)
     except subprocess.CalledProcessError as e:
         # Handle the case when Betty returns a non-zero exit code
-        pass
+
         # Append the error output to the common errors.txt file
         with open(output_file, 'a', encoding='utf-8') as errors_file_path:
             errors_file_path.write(e.stdout)
@@ -39,11 +41,11 @@ if __name__ == "__main__":
         sys.exit(1)
 
     # Specify the common errors.txt file in the current directory
-    errors_file_path = 'errors.txt'
+    ERRORS_FILE_PATH = 'errors.txt'
 
     # Clear the content of the errors.txt file before appending new errors
-    open(errors_file_path, 'w', encoding='utf-8').close()
+    open(ERRORS_FILE_PATH, 'w', encoding='utf-8').close()
 
     # Iterate over each file provided as a command-line argument
-    for file_path in sys.argv[1:]:
-        exctract_errors(file_path, errors_file_path)
+    for fpath in sys.argv[1:]:
+        exctract_errors(fpath, ERRORS_FILE_PATH)
