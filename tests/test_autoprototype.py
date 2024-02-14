a8fe53a4c668a042fbeca1fe213d8733c0b078d9
jonaahmed <23105954+jonaahmed@users.noreply.github.com>
2024-02-14 12:56:17 +0300

    Refactor autoprototype.py and add test cases

--- tests/test_autoprototype.py
+++ tests/test_autoprototype.py
@@ -1,19 +1,26 @@
-import unittest
-import pytest
+"""
+    Test the autoprototype.py file
+"""
 import os
+import pytest
 from colorama import Fore
-from bettyfixer.autoprototype import betty_check
+from bettyfixer.autoprototype import (betty_check,
+                                      print_check_betty_first,
+                                      #   print_header_name_missing,
+                                      #   print_ctags_header_error,
+                                      #   check_ctags
+                                      )
 
 
-class TestBettyCheck:
+class TestAutoprototypeSuite:
     """
-    Test the betty_check function from autoprototype.py
+    Test the autoprototype.py file
     """
 
     @pytest.fixture(autouse=True)
     def setup_method(self):
         """Set up the test """
-        with open("test.c", "w") as f:
+        with open("test.c", "w", encoding="utf-8") as f:
             f.write("int main() { return 0; }")
         yield
         os.remove("test.c")
@@ -22,22 +29,25 @@ class TestBettyCheck:
         """ Test if betty is installed"""
         assert betty_check() is not None
 
-    def test_betty_check_errors(self):
+    def test_betty_check_errors(self, capsys):
         """ Test if there are errors in the files
          Create a temporary file with errors"""
         betty = betty_check()
         assert not betty
+        captured = capsys.readouterr()
+        assert "exit status 1" in captured.out
 
     def test_betty_check_warnings(self):
         """ Test if there are warnings in the files
          Create a temporary file with warnings"""
         assert not betty_check()
 
-    def test_betty_check_no_errors(self):
-        """ Test if there are no errors in the files
-         Create a temporary file without errors"""
-        assert betty_check()
+    def test_print_check_betty_first(self, capsys):
+        """Test the print_check_betty_first function from autoprototype.py"""
+        expected_output = Fore.RED + \
+            "You should fix betty Errors first before copy prototype\
+ functions into The header file" + Fore.RESET + "\n"
 
-
-if __name__ == '__main__':
-    unittest.main()
+        print_check_betty_first()
+        captured = capsys.readouterr()
+        assert captured.out == expected_output
